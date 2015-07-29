89ce2482ed20897ec8cf987a9830753be2db3741
Matt Revelle <mrevelle@gmail.com>
2015-07-29 15:49:15 -0400

    Bumped snapshot version. Updated deps.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject notion "0.1.1"
+(defproject notion "0.1.2-SNAPSHOT"
   :description "A helper library for statistical machine learning."
   :url "https://github.com/mattrepl/notion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/math.combinatorics "0.0.8"]
-                 [prismatic/schema "0.4.1"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 [prismatic/schema "0.4.3"]])
