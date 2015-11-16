9f225b87b13c2ab6c9ac11bf8db5a9940b2125a6
Matt Revelle <mrevelle@gmail.com>
2015-11-16 11:53:58 -0500

    Updated dep versions.

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [prismatic/schema "0.4.3"]])
+                 [clojure.core.typed "0.3.18"]
+                 [munge "0.1.3-SNAPSHOT"]
+                 [prismatic/schema "1.0.3"]])
