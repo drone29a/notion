a2400f6bd969f2ac66458961ce5d63783b11139c
Matt Revelle <mrevelle@gmail.com>
2019-11-29 16:35:54 -0700

    Updated deps.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/mattrepl/notion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/core.typed "0.3.18"]
                  [munge "0.1.3-SNAPSHOT"]
                  [prismatic/schema "1.0.3"]])
